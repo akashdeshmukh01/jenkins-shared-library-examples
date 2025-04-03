b2e8cee00ac97bcaa13edb2f8fad5d918c7e7157
Akash Deshmukh <akashdeshmukh@Akashs-MacBook-Air.local>
2025-04-03 23:02:26 +0530

    added yaml reader

--- vars/loadParameters.groovy
+++ vars/loadParameters.groovy
@@ -1,10 +1,3 @@
-import org.yaml.snakeyaml.Yaml
-import java.nio.file.Files
-import java.nio.file.Paths
-
 def call() {
-    def yamlFilePath = "parameter.yaml"
-    def yamlText = new String(Files.readAllBytes(Paths.get(yamlFilePath)), "UTF-8")
-    def yaml = new Yaml()
-    return yaml.load(yamlText)
+    return readYaml(file: 'parameter.yaml')
 }
