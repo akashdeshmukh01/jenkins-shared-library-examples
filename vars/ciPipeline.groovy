7936a2e547f89e53ebf983c9da2117ef67fdd57c
Akash Deshmukh <akashdeshmukh@Akashs-MacBook-Air.local>
2025-04-03 22:36:22 +0530

    new changes

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,23 +1,45 @@
 def call() {
-    def params = readYaml file: 'parameters.yaml'
-    
-    stage('Checkout Code') {
-        checkout scm
-    }
-
-    stage('SonarQube Scan') {
-        sonarQubeScan(params.sonarqube)
-    }
-
-    stage('Trivy Scan') {
-        trivyScan(params.trivy)
-    }
-
-    stage('OWASP ZAP Scan') {
-        zapScan(params.zap)
-    }
-    
-    stage('Build & Push Docker Image') {
-        dockerBuildPush()
+    pipeline {
+        agent any
+        environment {
+            CONFIG = readYaml(file: 'parameter.yaml') // Read the YAML file once
+        }
+        stages {
+            stage('Checkout') {
+                steps {
+                    script {
+                        checkoutCode() // Calling shared library function for code checkout
+                    }
+                }
+            }
+            stage('SonarQube Analysis') {
+                steps {
+                    script {
+                        sonarQubeScan(CONFIG.sonarqube) // Pass SonarQube config
+                    }
+                }
+            }
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        buildDockerImage()
+                    }
+                }
+            }
+            stage('Trivy Scan') {
+                steps {
+                    script {
+                        trivyScan(CONFIG.trivy) // Pass Trivy config
+                    }
+                }
+            }
+            stage('Push to ECR') {
+                steps {
+                    script {
+                        pushDockerToECR()
+                    }
+                }
+            }
+        }
     }
 }
