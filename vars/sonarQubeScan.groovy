0e2e2da311da0dba3187c3ca3dc4f4609f765fd6
Akash Deshmukh <akashdeshmukh@Akashs-MacBook-Air.local>
2025-04-03 23:09:24 +0530

    added yaml reader

--- vars/sonarQubeScan.groovy
+++ vars/sonarQubeScan.groovy
@@ -1,22 +1,15 @@
-def call() {
-    script {
-        def params = loadParameters()
-        echo "Loaded Parameters: ${params}"  // Debugging output
+def call(Map sonarConfig) { 
+    if (!sonarConfig || !sonarConfig.projectKey || !sonarConfig.sources || !sonarConfig.hostUrl || !sonarConfig.authToken) {
+        error "Invalid or missing SonarQube configuration!"
+    }
 
-        if (!params?.sonarqube) {
-            error "SonarQube configuration is missing in parameter.yaml!"
-        }
-
-        def sonarConfig = params.sonarqube
-
-        withSonarQubeEnv('SonarQube') {
-            sh """
-                sonar-scanner \
-                -Dsonar.projectKey=${sonarConfig.projectKey} \
-                -Dsonar.sources=${sonarConfig.sources} \
-                -Dsonar.host.url=${sonarConfig.hostUrl} \
-                -Dsonar.login=${sonarConfig.authToken}
-            """
-        }
+    withSonarQubeEnv('SonarQube') {
+        sh """
+            sonar-scanner \
+            -Dsonar.projectKey=${sonarConfig.projectKey} \
+            -Dsonar.sources=${sonarConfig.sources} \
+            -Dsonar.host.url=${sonarConfig.hostUrl} \
+            -Dsonar.login=${sonarConfig.authToken}
+        """
     }
 }
