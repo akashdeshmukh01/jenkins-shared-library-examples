cbc0c17c83e8139bcf926527204711604d9d3f11
akashdeshmukh01 <74890480+akashdeshmukh01@users.noreply.github.com>
2025-04-03 22:42:41 +0530

    Update sonarQubeScan.groovy

--- vars/sonarQubeScan.groovy
+++ vars/sonarQubeScan.groovy
@@ -1,6 +1,12 @@
 def call() {
     script {
         def params = loadParameters()
+        echo "Loaded Parameters: ${params}"  // Debugging output
+
+        if (!params?.sonarqube) {
+            error "SonarQube configuration is missing in parameter.yaml!"
+        }
+
         def sonarConfig = params.sonarqube
 
         withSonarQubeEnv('SonarQube') {
