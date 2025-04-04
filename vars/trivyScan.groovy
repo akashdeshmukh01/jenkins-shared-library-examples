98d1f9423ec179ea49dd011c2fa86a56dfc7ea9c
Akash Deshmukh <akashdeshmukh@Akashs-MacBook-Air.local>
2025-04-04 11:15:49 +0530

    added yaml reader

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,17 +1,17 @@
-def call(Map config) {
+def call(String imageName) {
     script {
         try {
-            echo "Running Trivy scan on ${config.imageName}"
+            echo "Running Trivy scan on ${imageName}"
 
             def trivyCmd = """
-                trivy --config \$HOME/trivy.yaml --scanners vuln --ignorefile "" image ${config.imageName}
+                trivy image ${imageName}
             """
             def scanResult = sh(script: trivyCmd, returnStatus: true)
 
             if (scanResult != 0) {
-                error("Trivy scan failed! Vulnerabilities found in ${config.imageName}")
+                error("Trivy scan failed! Vulnerabilities found in ${imageName}")
             } else {
-                echo "Trivy scan passed! No critical vulnerabilities found in ${config.imageName}"
+                echo "Trivy scan passed! No critical vulnerabilities found in ${imageName}"
             }
         } catch (Exception e) {
             error "Trivy scan failed due to an error: ${e.message}"
